1beac3cf1bb35460a89c6337a7d6ec1699dc6479
Darren Shepherd <darren@rancher.com>
2016-05-16 21:29:51 -0700

    Strongswan 5.4.0

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e
 rm -rf dist
 mkdir -p dist/artifacts
 
-VERSION="5.3.5"
-PACKAGE="-2"
+VERSION="v5.4.0-rancher-1"
+PACKAGE="-1"
 
 docker build --build-arg VERSION=${VERSION} -t strongswan-build .
 docker run --rm strongswan-build cat strongswan-${VERSION}.tar.gz > dist/artifacts/strongswan-${VERSION}${PACKAGE}.tar.gz
