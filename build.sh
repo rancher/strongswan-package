7f326260ca073013ffab77ed42122cf1fac97a5e
Darren Shepherd <darren@rancher.com>
2016-06-02 14:57:37 -0700

    Rollback to 14.04

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -rf dist
 mkdir -p dist/artifacts
 
 VERSION="v5.4.0-rancher-2"
-PACKAGE="-1"
+PACKAGE="-2"
 
 docker build --build-arg VERSION=${VERSION} -t strongswan-build .
 docker run --rm strongswan-build cat strongswan-${VERSION}.tar.gz > dist/artifacts/strongswan-${VERSION}${PACKAGE}.tar.gz
